59f0fa7ad017f39753b220fbc262e76a93f88f88
Daria Hannochenko <hannochenko.pn@ucu.edu.ua>
2023-11-06 11:22:38 +0200

    Implemented docstrins, doctests and type hinting for function

---
+++ puzzle.py
@@ -0,0 +1,16 @@
+def validate_board(board: list) -> bool:
+    """
+    (list) -> bool
+    The function checks if game board ready to game.
+    If the board ready it returns True, otherwise returns False
+    >>> validate_board(["**** ****", "***1 1****", "**  3****", \
+"* 4 7****", "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    False
+    >>> validate_board(["**** ****", "***1 5****", "**  3****", \
+"* 4 7****", "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    True
+    >>> validate_board(["**** ****", "***1 ****", "**  3****", "* 4 1****", \
+"     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    False
+    """
+    pass
